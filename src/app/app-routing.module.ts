1c8bdff806cdfff1c7d7b3115f25427fa6858bc1
Kayla Miller <kayla@kaylamiller.dev>
2021-09-20 20:09:16 -0400

    Updated the routing to include routes for the login and signup pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
-import {AppComponent} from './app.component';
 import {HomeComponent} from './pages/home/home.component';
+import {LoginComponent} from "./pages/login/login.component";
+import {SignupComponent} from "./pages/signup/signup.component";
 
 const routes: Routes = [
-  { 
+  {
     path: '',
     component: HomeComponent
   },
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'signup',
+    component: SignupComponent
   }
 ];
 
